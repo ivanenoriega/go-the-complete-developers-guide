a586ff7d0ba6f2429a24fe637d4fddc55c25b4bd
Ivan <ivanenoriega@gmail.com>
2019-05-18 09:20:36 -0300

    class 28

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingDeck := deal(cards, 5)
-
-	// hand.print()
-	// remainingDeck.print()
+	// One - Create a new deck and save it as my_cards
 	cards := newDeck()
-	cards.saveToFile("my_cards")
+	cards.saveToFile("my_cards") // Save a deck in a file
+
+	// Two - Create a new deck from the file my_cards
+	// cards := newDeckFromFile("my_cards")
+	// cards := newDeckFromFile("error_case") // Error case
+	cards.print()
 }
