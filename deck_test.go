d1258cd28ead5adae5967b316db40438a2fde145
Ivan Emiliano Noriega <ivanenoriega@gmail.com>
2019-05-18 21:32:39 -0300

    class 33

--- deck_test.go
+++ deck_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestNewDeck(t *testing.T) {
-	e := 16
 	d := newDeck()
 
-	if len(d) != e {
-		t.Errorf("Expected deck of length %d, but got %v", e, len(d))
+	// Length
+	l := len(d)
+	if l != 16 {
+		t.Errorf("Expected deck of length %d, but got %v", 16, l)
+	}
+
+	// First card
+	fc := "Ace of Spades"
+	if d[0] != fc {
+		t.Errorf("Expected first card to be %v, but got %v", fc, d[0])
+	}
+
+	// Last card
+	lc := "Four of Clubs"
+	if d[len(d)-1] != lc {
+		t.Errorf("Expected last card to be %v, but got %v", lc, d[len(d)-1])
 	}
 }
